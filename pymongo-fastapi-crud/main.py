58d56416fae8ea001de57d2e29a1ab4c71081136
Brenda J. Freire da Costa <brendajoycefreire@gmail.com>
2024-11-08 11:23:48 -0300

    Conecte-se ao seu cluster MongoDB Atlas

--- pymongo-fastapi-crud/main.py
+++ pymongo-fastapi-crud/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+config = dotenv_values(".env")
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to my PyMongoTest of LearningPymongoDB tutorial!"}
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
